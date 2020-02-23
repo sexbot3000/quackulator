5cc3b429e90a49c9aa6270555312fbc8384de948
Steven McCann <stevenmancans@gmail.com>
2020-02-23 18:46:11 +1100

    Button click now shows string on display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// functions for operators
+
 function add(a, b) {
     return a + b;
 }
@@ -22,3 +24,30 @@ function operate(operator, a, b) {
     case divide : return divide(a, b);
     }
 }
+
+// functions for showing display
+function display(number) {
+    displayValue += number;
+    if (displayValue == '0') {
+        displayValue = '';
+    showValue(displayValue);
+    }
+}
+
+function showValue() {
+    document.getElementById('showValue').textContent = displayValue;
+}
+
+// functions for button press
+const One = document.getElementById('1').addEventListener("click", function () {showValue(display('1'))});
+const Two = document.getElementById('2').addEventListener("click", function () {showValue(display('2'))});
+const Three = document.getElementById('3').addEventListener("click", function() {showValue(display('3'))});
+const Four = document.getElementById('4').addEventListener("click", function () {showValue(display('4'))});
+const Five = document.getElementById('5').addEventListener("click", function () {showValue(display('5'))});
+const Six = document.getElementById('6').addEventListener("click", function () {showValue(display('6'))});
+const Seven = document.getElementById('7').addEventListener("click", function () {showValue(display('7'))});
+const Eight = document.getElementById('8').addEventListener("click", function () {showValue(display('8'))});
+const Nine = document.getElementById('9').addEventListener("click", function () {showValue(display('9'))});
+const Zero = document.getElementById('0').addEventListener("click", function () {showValue(display('0'))});
+
+let displayValue = '';
